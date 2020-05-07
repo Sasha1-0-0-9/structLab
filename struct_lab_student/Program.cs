2f6bd57db9bc0d7880ac99f1fde374d8f7cd868a
Sasha <sashapysmenny@gmail.com>
2020-05-07 14:07:44 +0300

    first commit

---
+++ struct_lab_student/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
+
+namespace struct_lab_student
+{
+    partial class Program
+    {
+        static Student[] ReadData(string fileName)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    count++;
+                }
+            }
+            Student[] students = new Student[count];
+            StreamReader str = new StreamReader(fileName, Encoding.Default);
+            string line;
+            int i = 0;
+            while ((line = str.ReadLine()) != null)
+            {
+                students[i] = new Student(line);
+                i++;
+            }
+            str.Close();
+            return students;
+        }        
+        static double ToValue(char symbol)
+        {
+            if (symbol == '-')
+            {
+                return 2;
+            }
+            else
+            {
+                return char.GetNumericValue(symbol);
+            }
+        }
+        static void Average(Student[] studs) //виведення прізвищ студентів у яких 5 з інформатики + їх середній бал
+        {
+            double average;
+            foreach (Student info in studs)
+            {
+                average = 0;
+                if (ToValue(info.informaticsMark) == 5)
+                {
+                    average += (ToValue(info.mathematicsMark) + ToValue(info.physicsMark) + ToValue(info.informaticsMark)) / 3;
+                    Console.WriteLine("{0}       {1}", info.surName , average.ToString("0.00"));
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student[] studs = ReadData("data.txt");
+            Console.WriteLine("Прiзвища та середнi бали студентiв, якi мають 5 з iнформатики");
+            Average(studs);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ struct_lab_student/Student.cs
@@ -0,0 +1,33 @@
+﻿using System; 
+using System.Text;
+using System.Linq;
+
+namespace struct_lab_student
+{
+    struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char mathematicsMark;
+        public char physicsMark;
+        public char informaticsMark;
+        public int scholarship;
+
+        public Student(string lineWithAllData)
+        {
+            string[] data = lineWithAllData.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            surName = data[0];
+            firstName = data[1];
+            patronymic = data[2];
+            sex = Convert.ToChar(data[3]);
+            dateOfBirth = data[4];
+            mathematicsMark = Convert.ToChar(data[5]);
+            physicsMark = Convert.ToChar(data[6]);
+            informaticsMark = Convert.ToChar(data[7]);
+            scholarship = int.Parse(data[8]);
+        }
+    }
+}
